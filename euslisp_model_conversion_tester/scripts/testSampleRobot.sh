5d66a79dd6543aa61537be00b5311eced2518386
Kei Okada <kei.okada@gmail.com>
2015-12-11 20:20:49 +0900

    Trimmed the branch to only the jsk_model_tools sub directory

--- euslisp_model_conversion_tester/scripts/testSampleRobot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. $HOME/.bashrc
-
-ROBOT=SampleRobot
-rosrun euslisp_model_conversion_tester testROBOT.sh -v `rospack find openhrp3`/share/OpenHRP-3.1/sample/model/sample1.wrl -r ${ROBOT}
-
