eee56d71ab89e0c90b4878a11b32eb36e11441e0
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2017-02-18 23:08:53 +0900

    Trimmed the branch to only the eus_assimp sub directory

--- eusurdf/textured_models/apply_to_xacro.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-for d in `find $DIR  -maxdepth 1 -mindepth 1 -type d`; do
-  rosrun eusurdf urdf_to_xacro.py $d/model.urdf $d/model.urdf.xacro
-done
